a7c7b73d7a88172ea1ffef69d62ff39b3d23ae79
Matthias Wolf <matthias.wolf@cern.ch>
2013-02-18 14:52:09 +0000

    First TMVA variables...

--- TTM/TMVASampler.cc
+++ TTM/TMVASampler.cc
@@ -26,14 +26,28 @@ void TTM_TMVASampler::FillTree(TTree* iTree, Process const * iProcess){
 	// Define variables going into the trees
 	float Tau1Pt;
 	float Tau2Pt;
-	float Tau1Eta;
-	float Tau2Eta;
+    unsigned int Tau1DecayMode;
+    unsigned int Tau2DecayMode;
+    unsigned int Tau1IsolationIndex;
+    unsigned int Tau2IsolationIndex;
+    float Tau1LTPt;
+    float Tau2LTPt;
+    unsigned int Tau1NProngs;
+    unsigned int Tau2NProngs;
+    float DitauVisibleMass;
 
 	// Define branches
 	iTree->Branch("Tau1Pt", &Tau1Pt);
 	iTree->Branch("Tau2Pt", &Tau2Pt);
-	iTree->Branch("Tau1AbsEta", &Tau1Eta);
-	iTree->Branch("Tau2AbsEta", &Tau2Eta);
+    iTree->Branch("Tau1DecayMode", &Tau1DecayMode);
+    iTree->Branch("Tau2DecayMode", &Tau2DecayMode);
+    iTree->Branch("Tau1IsolationIndex", &Tau1IsolationIndex);
+    iTree->Branch("Tau2IsolationIndex", &Tau2IsolationIndex);
+    iTree->Branch("Tau1LTPt", &Tau1LTPt);
+    iTree->Branch("Tau2LTPt", &Tau2LTPt);
+    iTree->Branch("Tau1NProngs", &Tau1NProngs);
+    iTree->Branch("Tau2NProngs", &Tau2NProngs);
+    iTree->Branch("DitauVisibleMass", &DitauVisibleMass);
 
 	// Instantiante Branches to read events more easily
 	TTMBranches* event = new TTMBranches(params, iProcess->GetNtuplePath());
@@ -56,10 +70,17 @@ void TTM_TMVASampler::FillTree(TTree* iTree, Process const * iProcess){
 		unsigned int bestCombo = event->bestCombo;
 
 		// Assign values
-		Tau1Pt				= event->TTM_Tau1Pt->at(bestCombo);
-		Tau2Pt				= event->TTM_Tau2Pt->at(bestCombo);
-		Tau1Eta				= fabs(event->TTM_Tau1Eta->at(bestCombo));
-		Tau2Eta				= fabs(event->TTM_Tau2Eta->at(bestCombo));
+		Tau1Pt = event->TTM_Tau1Pt->at(bestCombo);
+		Tau2Pt = event->TTM_Tau2Pt->at(bestCombo);
+        Tau1DecayMode = event->TTM_Tau1DecayMode->at(bestCombo);
+        Tau2DecayMode = event->TTM_Tau2DecayMode->at(bestCombo);
+        Tau1IsolationIndex = event->GetTau1IsolationIndex(bestCombo);
+        Tau2IsolationIndex = event->GetTau2IsolationIndex(bestCombo);
+        Tau1LTPt = event->TTM_Tau1LTPt->at(bestCombo);
+        Tau2LTPt = event->TTM_Tau2LTPt->at(bestCombo);
+        Tau1NProngs = event->TTM_Tau1NProngs->at(bestCombo);
+        Tau2NProngs = event->TTM_Tau2NProngs->at(bestCombo);
+        DitauVisibleMass = event->TTM_DitauVisibleMass->at(bestCombo);
 
 		// Fill tree
 		iTree->Fill();
