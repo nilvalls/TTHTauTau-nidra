81a993d615032b1041df42512897abc4cce54667
Matthias Wolf <matthias.wolf@cern.ch>
2013-02-18 18:57:53 +0000

    Add additional TMVA variables akin TTM/TMVASampler.cc.

--- TTE/TMVASampler.cc
+++ TTE/TMVASampler.cc
@@ -35,6 +35,16 @@ void TTE_TMVASampler::FillTree(TTree* iTree, Process const * iProcess){
     unsigned int Tau1NProngs;
     unsigned int Tau2NProngs;
     float DitauVisibleMass;
+	float DeltaRTau1Tau2;
+	float DeltaRTau1Lepton;
+	float DeltaRTau2Lepton;
+	float DeltaRTau1LeadingJet;
+	float DeltaRTau2LeadingJet;
+	float DeltaRTau1SubleadingJet;
+	float DeltaRTau2SubleadingJet;
+	float DeltaRLeptonLeadingJet;
+	float DeltaRLeptonSubleadingJet;
+	float LeadingJetSubleadingJetMass;
 
 	// Define branches
 	iTree->Branch("Tau1Pt", &Tau1Pt);
@@ -48,6 +58,16 @@ void TTE_TMVASampler::FillTree(TTree* iTree, Process const * iProcess){
     iTree->Branch("Tau1NProngs", &Tau1NProngs);
     iTree->Branch("Tau2NProngs", &Tau2NProngs);
     iTree->Branch("DitauVisibleMass", &DitauVisibleMass);
+    iTree->Branch("DeltaRTau1Tau2", &DeltaRTau1Tau2);
+    iTree->Branch("DeltaRTau1Lepton", &DeltaRTau1Lepton);
+    iTree->Branch("DeltaRTau2Lepton", &DeltaRTau2Lepton);
+    iTree->Branch("DeltaRTau1LeadingJet", &DeltaRTau1LeadingJet);
+    iTree->Branch("DeltaRTau2LeadingJet", &DeltaRTau2LeadingJet);
+    iTree->Branch("DeltaRTau1SubleadingJet", &DeltaRTau1SubleadingJet);
+    iTree->Branch("DeltaRTau2SubleadingJet", &DeltaRTau2SubleadingJet);
+    iTree->Branch("DeltaRLeptonLeadingJet", &DeltaRLeptonLeadingJet);
+    iTree->Branch("DeltaRLeptonSubleadingJet", &DeltaRLeptonSubleadingJet);
+    iTree->Branch("LeadingJetSubleadingJetMass", &LeadingJetSubleadingJetMass);
 
 	// Instantiante Branches to read events more easily
 	TTEBranches* event = new TTEBranches(params, iProcess->GetNtuplePath());
@@ -81,6 +101,53 @@ void TTE_TMVASampler::FillTree(TTree* iTree, Process const * iProcess){
         Tau1NProngs = event->TTE_Tau1NProngs->at(bestCombo);
         Tau2NProngs = event->TTE_Tau2NProngs->at(bestCombo);
         DitauVisibleMass = event->TTE_DitauVisibleMass->at(bestCombo);
+
+		float tau1eta	= event->TTE_Tau1Eta->at(bestCombo);
+		float tau1phi	= event->TTE_Tau1Phi->at(bestCombo);
+		float tau2eta	= event->TTE_Tau2Eta->at(bestCombo);
+		float tau2phi	= event->TTE_Tau2Phi->at(bestCombo);
+		float lepEta	= event->TTE_ElectronEta->at(bestCombo);
+		float lepPhi	= event->TTE_ElectronPhi->at(bestCombo);
+
+		float jet1eta = -99;
+		float jet1phi = -99;
+		float jet2eta = -99;
+		float jet2phi = -99;
+
+		DeltaRTau1Tau2 = DeltaR(tau1phi, tau1eta, tau2phi, tau2eta);
+		DeltaRTau1Lepton = DeltaR(tau1phi, tau1eta, lepPhi, lepEta);
+		DeltaRTau2Lepton = DeltaR(tau2phi, tau2eta, lepPhi, lepEta);
+
+		DeltaRTau1LeadingJet		= -99;
+		DeltaRTau2LeadingJet		= -99;
+		DeltaRTau1SubleadingJet		= -99;
+		DeltaRTau2SubleadingJet		= -99;
+		DeltaRLeptonLeadingJet		= -99;
+		DeltaRLeptonSubleadingJet	= -99;
+		LeadingJetSubleadingJetMass	= -99;
+
+		bool foundLeadingJet = false;
+		for(unsigned int j = 0; j < event->J_Pt->size(); j++){
+			float pt	= event->J_Pt->at(j);
+			float eta	= event->J_Eta->at(j);
+			float phi	= event->J_Phi->at(j);
+			TLorentzVector leadingJet(0,0,0,0); TLorentzVector subleadingJet(0,0,0,0);
+			if(DeltaR(phi, eta, tau1phi, tau1eta) < 0.25 || DeltaR(phi, eta, tau2phi, tau2eta) < 0.25 || DeltaR(phi, eta, lepPhi, lepEta) < 0.25){ continue; }
+			if(!foundLeadingJet){ // Fill leading jet
+				DeltaRTau1LeadingJet		= DeltaR(phi, eta, tau1phi, tau1eta);
+				DeltaRTau2LeadingJet		= DeltaR(phi, eta, tau2phi, tau2eta);
+				DeltaRLeptonLeadingJet		= DeltaR(phi, eta, lepPhi, lepEta);
+				leadingJet.SetPtEtaPhiM(pt, eta, phi, 0);
+				foundLeadingJet = true;
+			}else{ // Fill subleading jet
+				DeltaRTau1SubleadingJet		= DeltaR(phi, eta, tau1phi, tau1eta);
+				DeltaRTau2SubleadingJet		= DeltaR(phi, eta, tau2phi, tau2eta);
+				DeltaRLeptonSubleadingJet	= DeltaR(phi, eta, lepPhi, lepEta);
+				subleadingJet.SetPtEtaPhiM(pt, eta, phi, 0);
+				LeadingJetSubleadingJetMass = (leadingJet + subleadingJet).M();
+				break;
+			}
+		}
 
 		// Fill tree
 		iTree->Fill();
