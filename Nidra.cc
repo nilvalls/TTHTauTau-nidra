3ccf869ed39471892d9599a71d17c7e86a0d64f6
Nil Valls <nil@nilvalls.com>
2013-03-12 20:50:48 +0000

    Minor edit

--- Nidra.cc
+++ Nidra.cc
@@ -36,7 +36,7 @@ int main(int argc, char **argv){
 	if(IsArgumentThere("-o")){ Optimize();		}
 
 	if(IsArgumentThere("-t")){
-   	DistributeProcesses();
+		DistributeProcesses();
 		MakeTMVATrainingSample();
 	}
 
