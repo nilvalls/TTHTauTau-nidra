88abfec2acdd52ef86b5444db8118a0963cf0d92
Matthias Wolf <matthias.wolf@cern.ch>
2013-03-18 00:15:38 +0000

    Use ISO escape codes. #scramify

--- ColoredOutput.h
+++ ColoredOutput.h
@@ -1,14 +1,14 @@
 #ifndef ColoredOutput_h
 #define ColoredOutput_h
 
-#define GRAY    "\033[0;30m"
-#define RED     "\033[0;31m"
-#define GREEN   "\033[0;32m"
-#define BLUE    "\033[0;34m"
-#define ORANGE  "\033[0;33m"
-#define PURPLE  "\033[0;35m"
-#define CYAN    "\033[0;36m"
-#define WHITE   "\033[1;37m"
+#define GRAY     "\033[0;30m"
+#define RED      "\033[0;31m"
+#define GREEN    "\033[0;32m"
+#define BLUE     "\033[0;34m"
+#define ORANGE   "\033[0;33m"
+#define PURPLE   "\033[0;35m"
+#define CYAN     "\033[0;36m"
+#define WHITE    "\033[1;37m"
 #define BGRAY    "\033[0;40m"
 #define BRED     "\033[0;41m"
 #define BGREEN   "\033[0;42m"
@@ -17,6 +17,6 @@
 #define BPURPLE  "\033[0;45m"
 #define BCYAN    "\033[0;46m"
 #define BWHITE   "\033[1;47m"
-#define NOCOLOR "\e[0m"
+#define NOCOLOR  "\033[0m"
 
 #endif
